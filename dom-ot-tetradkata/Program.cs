4f3f217ee199079ee389c76aa2833ec9affc3420
Teodor168 <teodor16iliev@gmail.com>
2024-03-21 21:10:10 +0200

    neshto

---
+++ dom-ot-tetradkata/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace dom_ot_tetradkata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dai duma: ");
+            string input = Console.ReadLine();
+            int mid = input.Length / 2;
+            bool isPalindrome = true;
+            for (int i = 0; i < mid; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome)
+            {
+                Console.WriteLine("Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Ne e palindrom");
+            }
+        }
+    }
+}
